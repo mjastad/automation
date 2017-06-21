317d02bf0bb2896bf538ffb5832b6a34ac47ce43
Michael Jastad <mjastad@netapp.com>
2017-06-20 22:00:45 -0700

    Add files via upload

--- Task.py
+++ Task.py
@@ -5,5 +5,6 @@ class Task:
   def __init__(self, inst):
      self.instance = inst
 
+  @property
   def id(self):
      return self.instance['task_uuid']
--- VirtualMachine.py
+++ VirtualMachine.py
@@ -5,17 +5,22 @@ class VirtualMachine:
   def __init__(self, inst):
      self.instance = inst
 
+  @property
   def name(self):
      return self.instance['name']
 
+  @property
   def description(self):
      return self.instance['description']
 
+  @property
   def uuid(self):
      return self.instance['uuid']
 
+  @property
   def powerState(self):
      return self.instance['power_state']
 
+  @property
   def memory(self):
      return self.instance['memory_mb']
