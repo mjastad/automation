5a01d32a8102e5c93025c4dc50c3f0469ebf0469
Michael Jastad <mjastad@netapp.com>
2017-06-26 12:48:51 -0700

    Update User.py

--- solution/core/User.py
+++ solution/core/User.py
@@ -1,4 +1,9 @@
+!/usr/bin/env python
+
+''' 
+Class User: A non-mutable object designed to represent a user of a host RESTful endpoint.
 '''
+
 __author__ = "M. Jastad"
 __copyright__ = "Copyright 2017, CSRA Hackathon"
 __credits__ = ["Roger Gibson", "Dan Fallon"]
@@ -7,7 +12,7 @@ __version__ = "1.0.1"
 __maintainer__ = "M. Jastad"
 __email__ = "michael.jastad@nutanix.com"
 __status__ = "Reference"
-'''
+
 
 class User:
 
