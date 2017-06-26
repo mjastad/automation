5bdae4fe51214d020821bcbbde1a29742bb69541
Michael Jastad <mjastad@netapp.com>
2017-06-26 12:41:54 -0700

    Update Task.py

--- solution/model/Task.py
+++ solution/model/Task.py
@@ -1,4 +1,9 @@
+#!/usr/bin/env python
+
+''' 
+Class Task: A non-mutable container object designed around NTNX REST endpiint Task resource.
 '''
+
 __author__ = "M. Jastad"
 __copyright__ = "Copyright 2017, CSRA Hackathon"
 __credits__ = ["Roger Gibson", "Dan Fallon"]
@@ -7,7 +12,6 @@ __version__ = "1.0.1"
 __maintainer__ = "M. Jastad"
 __email__ = "michael.jastad@nutanix.com"
 __status__ = "Reference"
-'''
 
 class Task:
 
