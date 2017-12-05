e4119af19edbe66e6903db99a7404e8cdb4f085a
Michael Jastad <mjastad@netapp.com>
2017-12-05 13:27:43 -0800

    Update Service.py

--- solution/v2/core/Service.py
+++ solution/v2/core/Service.py
@@ -1,4 +1,13 @@
 
+_author__ = "M. Jastad"
+__copyright__ = "Copyright 2017, CSRA Hackathon"
+__credits__ = ["M.Jastad", "M.Lavi"]
+__license__ = "N/A"
+__version__ = "1.0.1"
+__maintainer__ = "M. Jastad"
+__email__ = "michael.jastad@nutanix.com"
+__status__ = "Reference"
+
 class Service:
 
    def __init__(self):
