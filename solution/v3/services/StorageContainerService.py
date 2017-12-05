f70c965f72ed6a843504b8999136b1bcf4a7816d
Michael Jastad <mjastad@netapp.com>
2017-12-05 15:34:10 -0800

    Update StorageContainerService.py

--- solution/v3/services/StorageContainerService.py
+++ solution/v3/services/StorageContainerService.py
@@ -1,5 +1,14 @@
 from v3.model.StorageContainer import StorageContainer
 
+__author__ = "M. Jastad"
+__copyright__ = "Copyright 2017, Calm Workshop"
+__credits__ = ["Chris Brown", "M.Lavi"]
+__license__ = "N/A"
+__version__ = "2.0.1"
+__maintainer__ = "M. Jastad"
+__email__ = "michael.jastad@nutanix.com"
+__status__ = "Reference"
+
 
 class StorageContainerService:
 
