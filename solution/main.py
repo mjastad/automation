c5471869560a78cb4b07ac7af56e8584440c3e27
Michael Jastad <mjastad@netapp.com>
2017-12-05 12:56:13 -0800

    Add files via upload

--- solution/main.py
+++ solution/main.py
@@ -1,12 +1,12 @@
-'''
+
 from v2.core.Connection import Connection
 from v2.core.Host import Host
 from v2.core.User import User
 from v2.services.VirtualMachineService import VirtualMachineService
 from v2.services.ImageService import ImageService
 from v2.services.StorageContainerService import StorageContainerService
-'''
 
+'''
 from v3.core.Connection import Connection
 from v3.core.Host import Host
 from v3.core.User import User
@@ -15,30 +15,30 @@ from v3.services.ImageService import ImageService
 from v3.services.StorageContainerService import StorageContainerService
 from v3.services.ApplicationService import ApplicationService
 from v3.services.BlueprintService import BlueprintService
+'''
 
 USER = "admin"
-#PASSWD = "passw0rd"
-#IPADDRESS = "10.68.69.102"
-PASSWD = "nx2Tech254!"
-IPADDRESS = "10.21.71.39"
+PASSWD = "passw0rd"
+IPADDRESS = "10.68.69.102"
+#PASSWD = "nx2Tech254!"
+#IPADDRESS = "10.21.71.39"
 PORT = "9440"
 
-def getVirtualMachines(connection, data):
-    vmService = VirtualMachineService()
-    vmList = vmService.getVMS(connection, data)
+def _virtualMachines(connection):
+    vmList = VirtualMachineService().getVMS(connection)
+    for vm in vmList : vm.show() 
 
+
+def getVirtualMachines(connection, data):
+    vmList = VirtualMachineService().getVMS(connection, data)
     for vm in vmList : vm.show() 
 
 def getApplications(connection, data):
-    appService = ApplicationService()
-    appList = appService.getApplications(connection, data)
-
+    appList = ApplicationService().getApplications(connection, data)
     for app in appList : app.show() 
 
 def getBlueprintss(connection, data):
-    bpService = BlueprintService()
-    bpList = bpService.getBlueprints(connection, data)
-
+    bpList = BlueprintService().getBlueprints(connection, data)
     for bp in bpList : bp.show()
      
 
@@ -50,9 +50,13 @@ def main():
     host = Host(IPADDRESS, PORT)
     connection = Connection(user, host)
 
-    getVirtualMachines(connection, data) 
-    getApplications(connection, data) 
-    getBlueprints(connection, data) 
+    #v2 API
+    _virtualMachines(connection) 
+
+    #v3 API
+    #getVirtualMachines(connection, data) 
+    #getApplications(connection, data) 
+    #getBlueprints(connection, data) 
 
 if __name__ == "__main__":
     main()
