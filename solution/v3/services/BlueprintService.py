22f37e6db1fac6539a90bfd49e6fd74b9a7d717d
Michael Jastad <mjastad@netapp.com>
2017-12-05 15:33:23 -0800

    Update BlueprintService.py

--- solution/v3/services/BlueprintService.py
+++ solution/v3/services/BlueprintService.py
@@ -2,6 +2,14 @@ from v3.model.Task import Task
 from v3.model.Blueprint import Blueprint 
 from v3.model.Response import Response 
 
+__author__ = "M. Jastad"
+__copyright__ = "Copyright 2017, Calm Workshop"
+__credits__ = ["Chris Brown", "M.Lavi"]
+__license__ = "N/A"
+__version__ = "2.0.1"
+__maintainer__ = "M. Jastad"
+__email__ = "michael.jastad@nutanix.com"
+__status__ = "Reference"
 
 class BlueprintService:
 
