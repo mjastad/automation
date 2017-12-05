1e8fcd02a3decf767837924f28f82bef1ee8d2f3
Michael Jastad <mjastad@netapp.com>
2017-12-05 14:05:12 -0800

    Update Connection.py

--- solution/v2/core/Connection.py
+++ solution/v2/core/Connection.py
@@ -1,7 +1,7 @@
 import json
 import requests
 
-_author__ = "M. Jastad"
+__author__ = "M. Jastad"
 __copyright__ = "Copyright 2017, CSRA Hackathon"
 __credits__ = ["M.Jastad", "M.Lavi"]
 __license__ = "N/A"
