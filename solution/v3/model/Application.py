340cd5b6e34526f1ae1b1b1e85add73b2d1c29f3
Michael Jastad <mjastad@netapp.com>
2017-12-05 15:30:53 -0800

    Update Application.py

--- solution/v3/model/Application.py
+++ solution/v3/model/Application.py
@@ -1,4 +1,13 @@
 
+__author__ = "M. Jastad"
+__copyright__ = "Copyright 2017, Calm Workshop"
+__credits__ = ["Chris Brown", "M.Lavi"]
+__license__ = "N/A"
+__version__ = "2.0.1"
+__maintainer__ = "M. Jastad"
+__email__ = "michael.jastad@nutanix.com"
+__status__ = "Reference"
+
 class Application:
 
   def __init__(self, inst):
