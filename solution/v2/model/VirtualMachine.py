efc8a4abc19efc6b7642f8a0f3db9fe7fddb33dd
Michael Jastad <mjastad@netapp.com>
2017-12-05 15:25:54 -0800

    Update VirtualMachine.py

--- solution/v2/model/VirtualMachine.py
+++ solution/v2/model/VirtualMachine.py
@@ -1,5 +1,12 @@
 
-
+__author__ = "M. Jastad"
+__copyright__ = "Copyright 2017, CSRA Hackathon"
+__credits__ = ["Roger Gibson", "Dan Fallon"]
+__license__ = "N/A"
+__version__ = "1.0.1"
+__maintainer__ = "M. Jastad"
+__email__ = "michael.jastad@nutanix.com"
+__status__ = "Reference"
 class VirtualMachine:
 
   def __init__(self, inst):
