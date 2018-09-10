f84742dc044954484679243fc51bd5eb2c660d44
Michael Jastad <cattfish00@gmail.com>
2018-09-10 14:06:42 -0700

    Update config.py

--- solution/config.py
+++ solution/config.py
@@ -1,9 +1,9 @@
-USER 	  	= "admin"
-PASSWD 	  	= "nx2Tech476!"
-IPADDRESS 	= "10.21.81.39"
-PORT 	  	= "9440"
+USER 	  	      = "<PRISM USER-ID>"
+PASSWD 	  	    = "<PRISM PASSWD>"
+IPADDRESS 	    = "<CLUSTER CVM IPADDRESS>"
+PORT 	  	      = "<CLUSTER CVM PRISM PORT>"
 
-VM 		= "PC"
+VM 		          = "<PRISM CENTRAL VM-NAME>"
 APPLICATION    	= "Launch_API_Lab2"
 BLUEPRINT      	= "Import_API_Lab"
 BLUEPRINT_FILE 	= "Import_API_Lab.json"
@@ -13,7 +13,7 @@ ACTIVE 	       	= "ACTIVE"
 DRAFT 	       	= "DRAFT"
 DELETED        	= "DELETED"
 
-CREDENTIAL     	= "CENTOS"
-PASSWORD     	= "nutanix/4u"
+CREDENTIAL     	= "<VM ID>"
+PASSWORD     	  = "<VM PASSWORD>"
 
 
